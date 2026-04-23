00ab2189ef0d20571f3b6eb7b8ba64872780f9d3
Marcin Dziewulski <hello@mobily.pl>
2026-04-23 11:57:44 +0200

    feat: add support for selectedVideoTrack prop on iOS

--- ios/Video/RCTVideoManager.m
+++ ios/Video/RCTVideoManager.m
@@ -13,6 +13,7 @@ RCT_EXPORT_VIEW_PROPERTY(allowsExternalPlayback, BOOL);
 RCT_EXPORT_VIEW_PROPERTY(textTracks, NSArray);
 RCT_EXPORT_VIEW_PROPERTY(selectedTextTrack, NSDictionary);
 RCT_EXPORT_VIEW_PROPERTY(selectedAudioTrack, NSDictionary);
+RCT_EXPORT_VIEW_PROPERTY(selectedVideoTrack, NSDictionary);
 RCT_EXPORT_VIEW_PROPERTY(chapters, NSArray);
 RCT_EXPORT_VIEW_PROPERTY(paused, BOOL);
 RCT_EXPORT_VIEW_PROPERTY(muted, BOOL);
